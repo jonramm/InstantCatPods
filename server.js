9e702190bd4c6c4819d02230f1130961ffc71f16
Jon Ramm <jrgbone@gmail.com>
2022-02-21 10:29:19 -0700

    made connection

--- server.js
+++ server.js
@@ -2,13 +2,12 @@ require('dotenv').config();
 
 const PORT = process.env.PORT || 4000;
 
-
 const express = require('express');
 const app = express();
-
 const path = require('path');
+const mysql = require('mysql')
+const db = require('./dbcon')
 
-// const mysql = require('mysql')
 // const connection = mysql.createConnection({
 //     host: process.env.MYSQL_HOST,
 //     user: process.env.MYSQL_USER,
@@ -16,16 +15,27 @@ const path = require('path');
 //     database: process.env.MYSQL_DB
 // })
 
-// connection.connect()
+// connection.connect((err) => {
+//   if (err) throw err;
+//   console.log('Connected!')
+// })
 
 app.use(express.urlencoded({
     extended: true
   }));
 
-
 // app.use(express.static('public'));
 
 app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/api/get', (req, res) => {
+  db.query('SELECT * FROM users', (err, result) => {
+    if(err) {
+      console.log(err)
+    }
+    res.send(result)
+  })
+})
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
