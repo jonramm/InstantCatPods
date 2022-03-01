6eb0daa0c8596012f78832dc0127ee28369a43b1
Theo <34557859+wr41thx@users.noreply.github.com>
2022-02-28 20:23:51 -0800

    More secure db queries and some error handling
    Changed the format for sending sql and variables through db.query for create and destroy.  Also updated the error handling to match class examples.  Needs to be tested!

--- routes/create.js
+++ routes/create.js
@@ -2,28 +2,55 @@ const express = require('express')
 const router = express.Router()
 const db = require('../dbcon')
 
+// I changed the way insertions are done for the db.query
+// and updated the error handling to match the examples from class
 
 router.post('/users', (req, res) => {
-    db.query(`INSERT INTO users (first_name, last_name, screen_name, dob) VALUES ('${req.body.first_name}', '${req.body.last_name}', '${req.body.screen_name}', '${req.body.dob}');`, (err, result) => {
+	
+	inserts = [req.body.first_name, req.body.last_name, req.body.screen_name, req.body.dob];
+	
+	sql_insert_user = 
+	"INSERT INTO users (first_name, last_name, screen_name, dob) VALUES (?,?,?,?)";
+	
+    db.query(sql_insert_user, inserts, (err, result) => {
         if(err) {
           console.log(err)
+		  res.write(JSON.stringify(err));
+		  res.end();
         }
         res.send(result)
       })
 })
 
 router.post('/avatars', (req, res) => {
+	
   if (req.body.name && req.body.user_id) {
-    db.query(`INSERT INTO avatars (name, user_id) VALUES ('${req.body.name}', ${req.body.user_id});`, (err, result) => {
+	  
+	  inserts = [req.body.name, req.body.user_id]
+	  
+	  sql_insert_avatar = 
+	  "INSERT INTO avatars (name, user_id) VALUES (?,?)";
+	  
+    db.query(sql_insert_avatar, inserts, (err, result) => {
       if(err) {
-        console.log(err)
+        console.log(err);
+		res.write(JSON.stringify(err));
+		res.end();
       }
       res.send(result)
     })
   } else {
-    db.query(`INSERT INTO avatars (name) VALUES ('${req.body.name}');`, (err, result) => {
+	  
+	inserts = [req.body.name];
+	
+	sql_insert_avatar = 
+	"INSERT INTO avatars (name) VALUES (?)";
+	
+    db.query(sql_insert_avatar, inserts, (err, result) => {
       if(err) {
         console.log(err)
+		res.write(JSON.stringify(err));
+		res.end();
       }
       res.send(result)
     })
@@ -31,18 +58,34 @@ router.post('/avatars', (req, res) => {
 })
 
 router.post('/cosmetics', (req, res) => {
-  db.query(`INSERT INTO cosmetics (description, type, price) VALUES ('${req.body.description}', '${req.body.type}', ${req.body.price});`, (err, result) => {
+	
+	inserts = [req.body.description, req.body.type, req.body.price];
+	
+	sql_insert_cosmetics = 
+	"INSERT INTO cosmetics (description, type, price) VALUES (?,?,?)";
+
+	db.query(sql_insert_cosmetics, inserts, (err, result) => {
     if(err) {
       console.log(err)
+	  res.write(JSON.stringify(err));
+	  res.end();
     }
     res.send(result)
   })
 })
 
 router.post('/orders', (req, res) => {
-  db.query(`INSERT INTO orders (order_date, total, status, user_id) VALUES ('${req.body.order_date}', ${req.body.total}, '${req.body.status}', ${req.body.user_id});`, (err, result) => {
+	
+	inserts = [req.body.order_date, req.body.total, req.body.status, req.body.user_id];
+	
+	sql_insert_orders = 
+	"INSERT INTO orders (order_date, total, status, user_id) VALUES (?,?,?,?)";
+	
+	db.query(sql_insert_orders, inserts, (err, result) => {
     if(err) {
       console.log(err)
+	  res.write(JSON.stringify(err));
+	  res.end();
     }
     res.send(result)
   })
