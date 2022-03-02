da9f0193db7d5260709b12f40841abaaa003c12e
Jon Ramm <jrgbone@gmail.com>
2022-03-02 14:32:58 -0700

    fix server routes for React page refresh

--- server.js
+++ server.js
@@ -33,13 +33,13 @@ app.use(express.json())
 
 // app.use(express.static('public'));
 
-// app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.use('/retrieve', retrieve)
 app.use('/create', create)
 app.use('/destroy', destroy)
 
-app.get('/', function (req, res) {
+app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
